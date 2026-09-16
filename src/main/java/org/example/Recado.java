581e4bc4dc4dd858c5dedabce1555d7a9676ffe8
47339389857 <raphalima2204@gmail.com>
2026-09-16 16:08:15 -0300

    ola

--- src/main/java/org/example/Recado.java
+++ src/main/java/org/example/Recado.java
@@ -1,4 +1,30 @@
-package main.java.org.example;
+package org.example;
 
 public class Recado {
+    private final int id;
+    private final String autor;
+    private final String mensagem;
+
+    public Recado(int id, String autor, String mensagem){
+        this.id = id;
+        this.autor = autor;
+        this.mensagem = mensagem;
+    }
+
+    public int getId() {return id;}
+    public String getAutor() {return autor;}
+    public String getMensagem() {return mensagem;}
+
+public String paraJson(){
+        return "{\"id\":"+id
+                +",\"autor\":\""+ autor + "\""
+                + ",\"mensagem\":\"" + mensagem + "\"}";
+}
+
+private String escapar (String texto){
+        return texto.replace("\\","\\\\")
+                .replace("\"","\\\"")
+                .replace("\r","")
+                .replace("\n", "\\n");
+}
 }
